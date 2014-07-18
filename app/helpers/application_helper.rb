f60c28bd0a09346e7f12c2d94afee955924cfb6e
lyntco <strawww@gmail.com>
2014-07-18 18:31:39 +1000

    Login/logout, seeds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def smartnav
+    links = link_to('Home', root_path) + " "
+    if @current_user
+      links += link_to('Games', games_path) + " "
+      links += link_to('Chats', chats_path) + " "
+      links += link_to("Logout #{@current_user.username}", login_path, :method => :delete)
+    else
+      links += link_to('Login', login_path) + " "
+      links += link_to('Sign up', new_user_path)
+    end
+    links
+  end
+
 end
