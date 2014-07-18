6cbf70e0fbb8840509d4238d46a23b4f55ac5686
TooDelirious <richfield14@gmail.com>
2014-07-18 16:27:41 +1000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root :to => 'sessions#new'
+  resources :chats do
+    resources :messages, :only => [:new, :create]
+  end
+
+  resources :games do
+    resources :messages, :only => [:new, :create]
+  end
+  resources :users
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
 
 end
