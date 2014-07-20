797e72a4d3ef6ff71f2dc8016b7bccf8542df967
TooDelirious <richfield14@gmail.com>
2014-07-20 16:45:55 +1000

    shut your face larlyn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
 
   def smartnav
-    links = link_to('Home', root_path) + " "
+    links = "<li>" + link_to('Home', root_path) + "</li>"
     if @current_user
-      links += link_to('Games', games_path) + " "
-      links += link_to('Chat to FireBall', chats_path) + " "
-      links += link_to("Settings", edit_user_path(@current_user.id)) + " "
-      links += "You are speaking " + LANGUAGES.invert[@current_user.native_language]
-      links += link_to("Logout #{@current_user.username}", login_path, :method => :delete)
+      links += "<li>" + link_to('Games', games_path) + " " + "</li>"
+      links += "<li>" + link_to('Chat to FireBall', chats_path) + " " + "</li>"
+      links += "<li>" + link_to("Settings", edit_user_path(@current_user.id)) + " " + "</li>"
+      links += "<li>" + "You are speaking " + LANGUAGES.invert[@current_user.native_language] + "</li>"
+      links += "<li>" + link_to("Logout #{@current_user.username}", login_path, :method => :delete) + "</li>"
     else
-      links += link_to('Login', login_path) + " "
-      links += link_to('Sign up', new_user_path)
+      links += "<li>" + link_to('Login', login_path) + " " + "</li>"
+      links += "<li>" + link_to('Sign up', new_user_path) + "</li>"
     end
     links
   end
