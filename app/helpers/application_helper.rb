3181c981b843de0c11bfa678c1756a9d96275019
TooDelirious <richfield14@gmail.com>
2014-07-20 18:24:04 +1000

    MORE CSS UPDATES

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
       links += "<li>" + link_to('Games', games_path) + " " + "</li>"
       links += "<li>" + link_to('Chat to FireBall', chats_path) + " " + "</li>"
       links += "<li>" + link_to("Settings", edit_user_path(@current_user.id)) + " " + "</li>"
-      links += "<li>" + "You are speaking " + LANGUAGES.invert[@current_user.native_language] + "</li>"
+      links += "<li class='invisible'>" + "You are speaking " + LANGUAGES.invert[@current_user.native_language] + "</li>"
       links += "<li>" + link_to("Logout #{@current_user.username}", login_path, :method => :delete) + "</li>"
     else
       links += "<li>" + link_to('Login', login_path) + " " + "</li>"
