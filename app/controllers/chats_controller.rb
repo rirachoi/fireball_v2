a03c70ff6d90a03e480e7fa794beeb221fd70e90
TooDelirious <richfield14@gmail.com>
2014-07-20 18:35:06 +1000

    updated fonts(test) + cleaned up nav

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,7 +4,7 @@ class ChatsController < ApplicationController
   def index
     @chats = @current_user.chats.order(updated_at: :desc)
     @language_select = LANGUAGES
-    @language_select.invert.delete(chat.language)
+    # @language_select.invert.delete(chat.language)
   end
 
   def create
