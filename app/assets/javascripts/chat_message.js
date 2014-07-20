1c46d8f1ef1c9281113eb02445ec7430ec7dfe66
TooDelirious <richfield14@gmail.com>
2014-07-20 16:58:19 +1000

    no input clickable fix

--- app/assets/javascripts/chat_message.js
+++ app/assets/javascripts/chat_message.js
@@ -5,29 +5,34 @@ $(document).ready(function(){
     var createChatMessage = function(event) {
       event.preventDefault();
       var $inputText = $('#input_text').val();
-      $.ajax({
-        url: '/chats/' + $currentChatId + '/messages',
-        method: 'post',
-        dataType: 'json',
-        data: {
-          input_text: $inputText,
-        },
-        success: function (response) {
-          var $chatBox = $('<div/>');
-          $chatBox.attr('id', 'chat_box');
-          $chatBox.addClass('animation-target');
-          var $newMsg = $('<li/>');
-          $newMsg.text(response.created_at + ": "+ response.input_text);
-          var $newTranslation = $('<li/>');
-          $newTranslation.addClass('translation');
-          $newTranslation.text(response.translation);
-          $newMsg.appendTo($chatBox);
-          $newTranslation.appendTo($chatBox);
-          $('#chat_messages').append($chatBox);
-          $('#input_text').val('').focus();
-          $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-        }
-      });
+      if ($inputText === ''){
+        return false
+      } else {
+
+        $.ajax({
+          url: '/chats/' + $currentChatId + '/messages',
+          method: 'post',
+          dataType: 'json',
+          data: {
+            input_text: $inputText,
+          },
+          success: function (response) {
+            var $chatBox = $('<div/>');
+            $chatBox.attr('id', 'chat_box');
+            $chatBox.addClass('animation-target');
+            var $newMsg = $('<li/>');
+            $newMsg.text(response.created_at + ": "+ response.input_text);
+            var $newTranslation = $('<li/>');
+            $newTranslation.addClass('translation');
+            $newTranslation.text(response.translation);
+            $newMsg.appendTo($chatBox);
+            $newTranslation.appendTo($chatBox);
+            $('#chat_messages').append($chatBox);
+            $('#input_text').val('').focus();
+            $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+          }
+        });
+      };
     };
 
   $('#speak').on('click', createChatMessage);
