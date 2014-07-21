2e294f9217bd8df9770daa4d307c4b43b60a91cd
lyntco <strawww@gmail.com>
2014-07-21 18:00:43 +1000

    Add completed column to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718002515) do
+ActiveRecord::Schema.define(version: 20140721075723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140718002515) do
     t.integer  "stage"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "completed",        default: false
   end
 
   create_table "messages", force: true do |t|
