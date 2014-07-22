5b11ed932c8abd39f2f6d28d33fcb6899b96a861
Rira Choi <rira.nunu.choi@gmail.com>
2014-07-23 09:55:33 +1000

    add wordlist

--- app/assets/javascripts/single_game.js
+++ app/assets/javascripts/single_game.js
@@ -123,7 +123,7 @@ $(document).ready(function() {
     // animation-target
     // showing answer
     var answerShow = function(){
-        var yourAnswer = $('<h2 id="answer_title">Answers</h2>')
+        var yourAnswer = $('<h2 id="answerTitle">Answers</h2>');
         var answerSlide = $('<div class="answerSlide">');
         var answerSheet = $('<div class="answerSheet">');
         answerSlide.append(yourAnswer);
@@ -143,12 +143,25 @@ $(document).ready(function() {
 //////////--------- word_list -----------///////////////
 
     // display word_list
+    var words = $('<div class="words"/>');
+
     var displayWordlist = function(){
+        var wordListTitle = $('<h2 id="wordListTitle">Words List</h2>');
+        var wordSlide = $('<div class="wordSlide"/>');
+        var wordSheet = $('<div class="wordSheet"/>');
+        wordSlide.append(wordListTitle);
+        words.append(wordSlide);
         for (var q=0; q<question.length; q++){
-            var $each_word = question[q];
-            display_text = "<h2>"+ $each_word +"</h2>"
-            $('#word_list').append(display_text);
+            var eachWord = question[q];
+            displayText = $("<h2 id='question'>"+ eachWord +"</h2>")
+            wordSheet.append(displayText);
+            wordSheet.prependTo(words);
+            $('body').append(words);
         };
+
+        $('.wordSlide').on('click', function(){
+            $('.wordSheet').slideToggle('slow');
+        });
     };
 //////////------- buttons setting -------///////////////
 
@@ -411,6 +424,7 @@ $(document).ready(function() {
         moveAnimatedbox();
         insertImages();
         setPlayImages();
+        displayWordlist();
         $('.peng_game').fadeIn('slow');
         $('.fireball_game').fadeIn('slow');
         loadPlayImage();
@@ -472,6 +486,7 @@ $(document).ready(function() {
         $msgEnd.append($replay);
         $msgEnd.append($learnMore);
 
+        $('.words').hide();
         $('#ani_container').empty();
         $('#ani_container').append($msgEnd);
         $('.msgEnd').show();
@@ -491,12 +506,10 @@ $(document).ready(function() {
             $('#btnplay').show();
             $('.pengLives').show();
 
+            $('.answers').hide();
             startPlay();
 
         });
-        // $('#learnMore').on('click', function(){
-        //     l;
-        // });
 
 
     }; // end endplay
@@ -504,7 +517,6 @@ $(document).ready(function() {
 
 
 /////////------button and click for calling functions--------///////////
-
     $('#btnsubmit').on('click', matchAnswer);
     $('#userInput').on('keydown', function(key){
         if (key.which == 13){
