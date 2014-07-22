d6781e7c6698d297b78d83ad6e659116f89a28a8
TooDelirious <richfield14@gmail.com>
2014-07-22 10:55:11 +1000

    new nav buttons!!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,52 @@ module ApplicationHelper
 
   def smartnav
 
-    links = '<li>' + link_to('Fireball', root_path) + "</li>"
+    links = '<li>'
+    links += link_to(root_path) do
+              image_tag('icons/fireball.png', :class => 'thumb')
+            end
+    links +="</li>"
 
     if @current_user
-      links += "<li>" + link_to('Games', games_path) + " " + "</li>"
-      links += "<li>" + link_to('Chat to FireBall', chats_path) + " " + "</li>"
-      links += "<li>" + link_to("Settings", edit_user_path(@current_user.id)) + " " + "</li>"
-      links += "<li class='invisible'>" + "You are speaking " + LANGUAGES.invert[@current_user.native_language] + "</li>"
-      links += "<li>" + link_to("Logout #{@current_user.username}", login_path, :method => :delete) + "</li>"
+      links += "<li>"
+      links += link_to(games_path) do
+                image_tag('icons/controller.png', :class => 'thumb')
+              end
+      links += "</li>"
+
+      links += "<li>"
+      links += link_to(chats_path) do
+               image_tag('icons/chat.png', :class => 'thumb')
+             end
+      links += "</li>"
+
+      links += "<li>"
+      links += link_to (edit_user_path(@current_user.id)) do
+                image_tag('icons/bigcog.png', :class => 'settings_tag')
+              end
+      links +="</li>"
+
+
+      # links += "<li class='invisible'>" + "You are speaking " + LANGUAGES.invert[@current_user.native_language] + "</li>"
+
+
+      links += "<li>"
+      links += link_to(login_path, :method => :delete) do
+                image_tag('icons/logout.png', :class => 'login_tag')
+              end
+      links +="</li>"
     else
-      links += "<li>" + link_to('Login', login_path) + " " + "</li>"
-      links += "<li>" + link_to('Sign up', new_user_path) + "</li>"
+      links += "<li>"
+      links += link_to(login_path) do
+                image_tag('icons/login.png', :class => 'login_tag')
+              end
+      links += "</li>"
+
+      links += "<li>"
+      links += link_to('Sign up', new_user_path) do
+                image_tag('icons/logout.png', :class => 'thumb')
+              end
+      links +="</li>"
     end
     links
   end
