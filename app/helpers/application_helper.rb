6e71b2e2502cb9d755bbdb42ce2a43b710f4cad3
TooDelirious <richfield14@gmail.com>
2014-07-22 12:07:19 +1000

    flippers is still broken -_-

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,48 +14,96 @@ module ApplicationHelper
     links += "<div class='back'>"
     links += link_to('Fireball', root_path)
     links += "</div>"
+    links += "</div>"
+    # links += "</div>"
 
     if @current_user
-      links += "<div>"
+      # links = "<div class='flip-container'>"
+      links += "<div =class'flipper'>"
+      links += "<div class='front'>"
       links += link_to(games_path) do
                 image_tag('icons/controller.png', :class => 'thumb')
               end
       links += "</div>"
 
-      links += "<div>"
+      links += "<div class='back'>"
+      links += link_to('Game', games_path)
+      links += "</div>"
+      links += "</div>"
+      # links += "</div>"
+
+      # links = "<div class='flip-container'>"
+      links += "<div =class'flipper'>"
+      links += "<div class='front'>"
       links += link_to(chats_path) do
                image_tag('icons/chat.png', :class => 'thumb')
              end
       links += "</div>"
 
-      links += "<div>"
+      links += "<div class='back'>"
+      links += link_to('Chat', chats_path)
+      links += "</div>"
+      links += "</div>"
+      # links += "</div>"
+
+      # links = "<div class='flip-container'>"
+      links += "<div =class'flipper'>"
+      links += "<div class='front'>"
       links += link_to (edit_user_path(@current_user.id)) do
                 image_tag('icons/bigcog.png', :class => 'settings_tag')
               end
       links +="</div>"
 
+      links += "<div class='back'>"
+      links += link_to('Settings', edit_user_path(@current_user.id))
+      links += "</div>"
+      links += "</div>"
+      # links += "</div>"
+
+
 
       # links += "<li class='invisible'>" + "You are speaking " + LANGUAGES.invert[@current_user.native_language] + "</li>"
 
-
-      links += "<div>"
+      # links = "<div class='flip-container'>"
+      links += "<div =class'flipper'>"
+      links += "<div class='front'>"
       links += link_to(login_path, :method => :delete) do
                 image_tag('icons/logout.png', :class => 'login_tag')
               end
       links +="</div>"
+
+      links += "<div class='back'>"
+      links += link_to('Logout', login_path, :method => :delete)
+      links += "</div>"
+      links += "</div>"
+      # links += "</div>"
+
     else
-      links += "<div>"
+      # links = "<div class='flip-container'>"
+      links += "<div =class'flipper'>"
+      links += "<div class='front'>"
       links += link_to(login_path) do
                 image_tag('icons/login.png', :class => 'login_tag')
               end
       links += "</div>"
 
-      links += "<div>"
+      links += "<div class='back'>"
+      links += link_to('Login', login_path)
+      links += "</div>"
+      links += "</div>"
+      # links += "</div>"
+
+      # links = "<div class='flip-container'>"
+      links += "<div =class'flipper'>"
+      links += "<div class='front'>"
       links += link_to(new_user_path) do
                 image_tag('icons/signup.png', :class => 'thumb')
               end
       links +="</div>"
 
+      links += "<div class='back'>"
+      links += link_to('Signup', new_user_path)
+      links += "</div>"
       links += "</div>"
       links += "</div>"
     end
