0af9fdb0db837dd548ed759aa4c0caf536ea3c66
lyntco <strawww@gmail.com>
2014-07-23 11:13:46 +1000

    Begin FRIENDSHIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721075723) do
+ActiveRecord::Schema.define(version: 20140723010723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20140721075723) do
     t.string   "language"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.boolean "approved"
   end
 
   create_table "games", force: true do |t|
